6c3295e43204ebb54786dffe05ac8c525b26ec58
Fabian Reitzug <fabian.reitzug@web.de>
2023-07-06 23:51:41 +0100

    First version of gas stove dashboard

---
+++ code/test_dashboard_stoves.R
@@ -0,0 +1,89 @@
+
+library(leaflet)
+library(terra)
+library(raster)
+library(magrittr)
+library(dplyr)
+library(sf)
+library(shiny)
+
+
+#pre processing plumes 
+
+shp <- st_read("../data/Boundaries/cb_2018_us_state_20m/cb_2018_us_state_20m.shp")
+shp <- st_transform(shp,"+proj=longlat +datum=WGS84")
+shp$gas <- rnorm(nrow(shp), mean=.38, sd=.2) #simulate prevalence of gas cook stoves in the US
+shp %<>% mutate(gas_cat = case_when(gas <.3 ~1,
+                                    gas >= .3 & gas <.6 ~2,
+                                    gas >.6 ~3
+)) %>% filter(NAME!="Hawaii" & NAME!="Alaska" )
+shp$gas_cat <- as.factor(shp$gas_cat)
+levels(shp$gas_cat) <- c("<30%","30-60%","60+%")
+
+#merge in cdc dat
+cdc <- read.delim("../data/CDC/Underlying Cause of Death, 2018-2021, Single Race-2.txt")[-1]
+cdc$State.Code <- ifelse(nchar(as.character(cdc$State.Code))==1,paste0("0",cdc$State.Code),cdc$State.Code)
+cdc <- merge(shp,cdc,by.x="GEOID",by.y="State.Code")
+cdc$Crude.Rate[cdc$Crude.Rate=="Unreliable"] <- NA
+
+col <- colorQuantile("YlOrRd",cdc$gas)
+
+#by race
+cdc <- read.delim("../data/CDC/Underlying Cause of Death, 2018-2021, Single Race-3.txt")[-1]
+cdc$State.Code <- ifelse(nchar(as.character(cdc$State.Code))==1,paste0("0",cdc$State.Code),cdc$State.Code)
+cdc <- merge(shp,cdc,by.x="GEOID",by.y="State.Code")
+cdc$Crude.Rate[cdc$Crude.Rate=="Unreliable"] <- NA
+cdc$Crude.Rate <- cdc$Crude.Rate %>% as.numeric()
+
+
+cdc %<>% arrange(desc(Single.Race.6))
+
+
+### -------
+
+
+# Define UI
+
+ui <- navbarPage(
+  "ME-thane Dashboard",
+  tabPanel("Gas stoves",
+           fluidPage(
+             sidebarLayout(
+               sidebarPanel(width = 1,
+                            #sliderInput("nbuffer","Search for leaks. Set radius in km:", min = 1, max = 100, value = 12, step = 1),
+                            #fileInput("upload", NULL, buttonLabel = "Upload CSV", multiple = FALSE,accept=".csv")
+                            selectInput(inputId="filter",label="Select" ,choices = c("gas","Crude.Rate"),selected ="gas")
+               ),
+               mainPanel(
+                 column(width = 4,
+                 leafletOutput("map")
+                 ),
+                 column(width = 8,
+                 plotOutput("plot1")
+                 )
+               )
+             )
+           )
+  )
+)
+
+# Define server
+server <- function(input, output, session) {
+  
+  # Render Leaflet map
+  output$map <- renderLeaflet({
+    leaflet() %>% addTiles() %>% addPolygons(data=shp,fillColor= ~col(shp$gas),col="white",weight=1,popup = paste0(round(shp$gas*100,0),"% of households have gas stoves in ",shp$NAME)) %>%
+      addLegend(pal = col, values = shp$gas, group = "circles", position = "bottomleft",title="Households with gas stove")
+  })
+  output$plot1 <- renderPlot({
+      ggplot(cdc, aes(x = gas_cat, y = Crude.Rate, group=gas_cat,
+                    fill = Single.Race.6)) +
+      geom_bar(stat = "identity") +
+      labs(x = "US state-level use of gas stoves", y = "Child deaths (<15 years)")+
+      ggtitle("Gas stove use and child deaths \nfrom respiratory disease")+
+      theme_linedraw()
+  })
+}
+
+# Run the app
+shinyApp(ui = ui, server = server)
