9ffdb0c5c4f9091796c3f02c07b45092178369f1
Abram Schonfeldt <abram.schonfeldt@gmail.com>
2023-07-09 22:08:04 +0100

    Integrates lat, long functionality

--- code/methane_shiny/methane_shiny.R
+++ code/methane_shiny/methane_shiny.R
@@ -96,20 +96,22 @@ respiratory_by_state$month <- as.Date(respiratory_by_state$month, format = "%Y/%
 # Plume finder ui ---------------------------------------------------------
 
 
-plume_ui <- fluidPage(
-  titlePanel("Reported methane leaks by location"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      width = 3,
-      sliderInput("nbuffer","Search for leaks. Set radius in km:", min = 1, max = 100, value = 12, step = 1),
-      fileInput("upload", NULL, buttonLabel = "Upload CSV", multiple = FALSE,accept=".csv")
-    ),
-    
-    mainPanel(
-      leafletOutput("map")
-    )
-  )
+plume_ui <- div(
+  class="outer",
+  tags$head(
+    # Include our custom CSS
+    includeCSS("../styles.css"),
+    includeScript("../gomap.js")
+   ),
+   leafletOutput("map",height="100%"),
+   textOutput("test"),
+   absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                 draggable = TRUE, top = 120, left = "auto", right = 20, bottom = "auto",
+                 width = 330, height = "auto",
+                 sliderInput("nbuffer","Search for leaks. Set radius in km:", min = 1, max = 100, value = 12, step = 1),
+                 numericInput("lat", "Lat", NA),
+                 numericInput("lon", "Lon", NA)
+   )
 )
 
 # Buttons ui -----------------------------------------------------------
@@ -249,38 +251,50 @@ server <- function(input, output) {
   # Plume functionality -------------------
   
   # Reactive variables
+  
   #if file uploaded, replace pts
   pts <- reactive({
-    if(!is.null(input$upload)){
-      pts <- vroom::vroom(input$upload$datapath, delim = ",") %>% as.data.frame() %>%  st_as_sf(coords = c("lon","lat"),crs=4326)
+    if(!is.na(input$lon)){
+      pts <- matrix(data=c(input$lon,input$lat),1,2) %>% as.data.frame()
+      names(pts) <- c("lon","lat")
+      pts %<>% st_as_sf(coords = c("lon","lat"),crs=4326)
     }else{
-      pts <- pt0 # loaded in setup
+      pts <- pt0
     }
   })
   
-  nbuffer <- reactive({input$nbuffer*1000}) #covert to meter (needed for st_buffer)
-  buffer  <- reactive({st_buffer(pts(), dist = nbuffer())})
-  sel     <- reactive({st_within(df_sf,buffer(),sparse = F)})
-  num     <- reactive({length(sel()[sel()==TRUE])})
-  sf_out  <- reactive({df_sf[sel(),]})
+  nbuffer <- reactive({input$nbuffer*1000}) #covert to meter (neded for st_buffer)
+  buffer <- reactive({st_buffer(pts(), dist = nbuffer())})
+  sel <- reactive({st_within(df_sf,buffer(),sparse = F)})
+  num <- reactive({length(sel()[sel()==TRUE])})
+  sf_out <-  reactive({df_sf[sel(),]})
   
   lab <- reactive({
     if(num() > 0){
       lab <-paste0("There are ",num()," pipeline leaks within ", nbuffer()/1000, " km of your location. <br><br>You can lobby your \nlocal politician <b>Ms. Shelly Maine</b> to fix this issue. She can be reached at <b>1-515-616-777</b> or at <a href='shelly.main@local.gov.org'>shelly.main@local.gov.org</a>")
     }else{
-      lab <- paste0(
-        "There are no documented pipeline leaks within ", nbuffer()/1000, " km of your location <b>but this does not mean there are no methane leaks near you.</b> <br><br>Methane leak data are only available for parts of: ",
+      lab <- paste0("There are no documented pipeline leaks within ", nbuffer()/1000, " km of your location <b>but this does not mean there are no methane leaks near you.</b> <br><br>Methane leak data are only available for parts of: ",
                     paste(sort(c("California", "Arizona", "Colorado", "Utah", "New Mexico", "Texas", "Lousiana", "Pennsylvania", "West Virginia", "Ohio")),collapse=", "),
                     ". <br><br>Change the map layer to see where documented leaks are across the US. <br><br>Lobby your state representative to collect data on leaks in your state. Find your state representate: <a href='https://www.house.gov/representatives/find-your-representative'>here</a> (external website)."
       )
     }
   })
   
-  # Render Leaflet map
+  
+  # Get map from hover ------------------------------------------------------
+  
+  
+  observeEvent(input$lon>0, {
+    proxy <- leafletProxy("map")
+    proxy %>% clearMarkers() # Clear existing markers
+    proxy %>%  addTiles() %>% addCircleMarkers(data=sf_out(),radius= sf_out()$qplume) %>% addScaleBar() %>% addMeasure(primaryLengthUnit ="meters") %>%
+      addMarkers(data=pts(),popup =  lab(),layerId = 10) %>% addPolygons(data=buffer(),opacity = .01)
+  })
+  
+  # Render map --------------------------------------------------------------
   output$map <- renderLeaflet({
     leaflet() %>% addTiles() %>% addCircleMarkers(data=sf_out(),radius= sf_out()$qplume) %>% addScaleBar() %>% addMeasure(primaryLengthUnit ="meters") %>%
-      addMarkers(data=pts(),popup =  lab()) %>% addPolygons(data=buffer(),opacity = .01,label="Click on the marker to find out what you can do!")
-    
+      addMarkers(data=pts(),popup =  lab(),layerId = 10) %>% addPolygons(data=buffer(),opacity = .01)
   })
   
   
